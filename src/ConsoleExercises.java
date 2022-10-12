910c6d385377520e3087d263690bda1e8476a7c9
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-12 16:56:00 -0500

    control-flow

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,25 +1,25 @@
 import java.sql.SQLOutput;
-import java.util.Scanner;
-
-public class ConsoleExercises {
-
-    public static void main(String[] args) {
-        double pi = 3.14159;
+//import java.util.Scanner;
+//
+//public class ConsoleExercises {
+//
+//    public static void main(String[] args) {
+//        double pi = 3.14159;
 //        System.out.printf("The value of pi is approximately %.2f%n", pi);
 //
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 //        sc.useDelimiter("\n");
 //        System.out.println("Enter an integer:");
 //        int userInteger = sc.nextInt();
 //        System.out.printf("You entered %d%n", userInteger);
-        System.out.println("Enter three words:");
-        // String word1, word2, word3;
-        // word1 = sc.next();
+//        System.out.println("Enter three words:");
+//         String word1, word2, word3;
+//         word1 = sc.next();
 //        String word1 = sc.nextLine();
 //        String word2 = sc.next();
 //        String word3 = sc.next();
-        String word1 = sc.next(), word2 = sc.next(), word3 = sc.next();
-        System.out.printf("%s%n%s%n%s%n", word1, word2, word3);
+//        String word1 = sc.next(), word2 = sc.next(), word3 = sc.next();
+//        System.out.printf("%s%n%s%n%s%n", word1, word2, word3);
 //
 //        System.out.println("Enter a sentence:");
 //        String userSentence = sc.nextLine();
@@ -31,12 +31,12 @@ public class ConsoleExercises {
 //        double width = Double.parseDouble(sc.nextLine());
 //        System.out.println("Enter the height of your room in feet:");
 //        double height = Double.parseDouble(sc.nextLine());
-////        int perimeter = Integer.parseInt(length) * 2 + Integer.parseInt(width) * 2;
+//        int perimeter = Integer.parseInt(length) * 2 + Integer.parseInt(width) * 2;
 //        double perimeter = width * 2 + length * 2;
 //        double volume = width * height * length;
 //        System.out.printf("The perimeter of your classroom is %.2f square feet%n", perimeter);
 //        System.out.printf("The volume of your classroom is %.2f cubic feet%n", volume);
-
+//
 //        System.out.println("Enter the width of your classroom in feet:");
 //        int width = sc.nextInt();
 //        System.out.println("Enter the length of your classroom in feet:");
@@ -44,6 +44,6 @@ public class ConsoleExercises {
 //        System.out.printf("The perimeter of your classroom is %d%n", width* 2 + length* 2);
 //        String sentence = sc.nextLine();
 //        System.out.println(sentence);
-    }
-
-}
+//    }
+//
+//}
--- src/ControlStatementsLecture.java
+++ src/ControlStatementsLecture.java
@@ -1,36 +1,84 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+//
+//public class ControlStatementsLecture {
+//
+//    // Statements
+//    int myInteger = 20;
+//
+//    public static void main(String[] args) {
+//        int x = 6;
+//        if (x == 5) {
+//            System.out.println("x is 5");
+//        } else if (x > 5 && x < 15) {
+//            System.out.println("x is between 5 and 15");
+//        } else {
+//
+//        }
 
-public class ConsoleIOLecture {
-    public static void main(String[] args) {
-        String cohort = "Weywot";
-        String timeOfDay = "morning";
-        byte week = 10;
-        float someNumber = 123.456F;
 
-
-//        System.out.printf("Good, %s %s!%n", timeOfDay, cohort);
-//        System.out.printf("This is the %dth week of %s cohort.%n", week, cohort);
-//        System.out.println("Will this be on a separate line?");
-//        System.out.println("How about this?");
-//        System.out.printf("The number is %011.2f%n", someNumber);
-//        System.out.printf("The number is %-11.2f test%n", someNumber);
-//        System.out.printf("The number is %d%n", week);
-
+//        String customerChoice;
+//
 //        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter a number:");
-////        int userNumber = Integer.parseInt(scanner.nextLine());
-//        int userNumber = scanner.nextInt();
-//        System.out.printf("Your number was: %d%n", userNumber);
-//        System.out.println("Enter a sentence:");
-////        String ghost = scanner.nextLine();
-//        scanner.nextLine();
-////        String userInput = scanner.next() + scanner.nextLine();
-//        String userInput = scanner.nextLine();
-//        System.out.printf("Your sentence was %s.%n", userInput);
-//        System.out.println("Enter another sentence:");
-//        String secondSentence = scanner.nextLine();
-//        System.out.printf("Your second sentence was %s.%n", secondSentence);
+//        System.out.println("What flavor do you want?");
+//        customerChoice = scanner.next();
+
+        // traditional switch statement
+
+//        switch (customerChoice) {
+//            case "vanilla":
+//                System.out.println("One vanilla coming right up");
+//                break;
+//            case "chocolate":
+//                System.out.println("chocolate coming right up");
+//                break;
+//            case "strawberry":
+//                System.out.println("strawberry coming right up");
+//                break;
+//            default:
+//                System.out.println("We don't have that");
+//                break;
+//        }
+
+        // enhanced switch statement
+
+//        switch (customerChoice) {
+//            case "vanilla" -> System.out.println("One vanilla coming right up");
+//            case "chocolate" -> System.out.println("chocolate coming right up");
+//            case "strawberry" -> System.out.println("strawberry coming right up");
+//            default -> System.out.println("We don't have that");
+//        }
+
+//        int c = 0, d = 100, e = 50;
+//        if (c == 1 & e++ < 100) {
+//
+//        }
+//        System.out.println("e = " + e);
+//
+//
+//        boolean existence = true;
+//        int theAnswer = 42;
+//        if (existence) System.out.println("Yes");
+//        if (existence || ++theAnswer == 42) {
+//            System.out.println("True");
+//        }
+//        System.out.println(theAnswer);
+//        if (theAnswer++==42) {
+//            System.out.println("The answer is 42");
+//        } else {
+//            System.out.println("The answer is not 42");
+//        }
+//
+//        int z = 2;
+//        int a = 3;
+//        if (a == 3 | z==2) System.out.println("This is not going to happen");
+//
+//        int i = 0;
+//
+//        for(int i = 0; i < 10; i++){
+//            System.out.println(i);
+//        }
+//
+//    }
+//}
 
 
-
-    }
