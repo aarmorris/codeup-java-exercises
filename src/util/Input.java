5d25d7390ca1290c84212a5d25913c66ea7c47b2
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-18 16:27:23 -0500

    Input-Test

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,68 @@
 package util;
+import java.util.Scanner;
 
-class Input {
+public class Input {
+    private Scanner scanner;
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getString() {
+        System.out.println("Enter your input:");
+        return scanner.nextLine();
+    }
+
+
+    public boolean yesNo() {
+        System.out.println("Enter input y/n:");
+        String userInput = scanner.nextLine();
+        if (userInput.equals("y") || userInput.equals("yes")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public int getInt() {
+        System.out.println("Enter number of your choice:");
+        return scanner.nextInt();
+    }
+
+
+    public int getInteger(int min, int max) {
+        System.out.print("Enter a number between 1- 85: ");
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        if (input < min || input > max) {
+            System.out.println("That number is out of range please re-enter.");
+            return getInteger(min, max - 1);
+        } else {
+            return input;
+
+        }
+    }
+
+
+    public double getDouble(double min, double max) {
+        System.out.print("Enter a number with a decimal:");
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        if (input < min || input > max) {
+            System.out.println("That number is out of range please re-enter.");
+            return getDouble(min, max);
+        } else {
+            return input;
+        }
+    }
+
+    public Input() {
+        scanner = new Scanner(System.in);
+    }
+
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,10 @@
+package util;
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println(input.getString());
+        input.getInteger(1,85);
+        System.out.println(input.yesNo());
+        System.out.println(input.getInt());
+    }
+}
